35358615e24239bafc1d6136aff2f293eb451f77
Vaishnav Puram <puramv307@gmail.com>
2024-06-15 19:20:49 +0530

    init customer-service

---
+++ services/customer-service/src/test/java/com/ecomm/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
