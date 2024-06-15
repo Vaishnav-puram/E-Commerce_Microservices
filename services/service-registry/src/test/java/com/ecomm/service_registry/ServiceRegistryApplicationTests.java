847e5dc30e8be71dc83d922e93039522c0102e9b
Vaishnav Puram <puramv307@gmail.com>
2024-06-15 18:52:33 +0530

    init service-registry

---
+++ services/service-registry/src/test/java/com/ecomm/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
